e4fb42aaea153d468d884232fe9b22a7066a1237
choongnyeong6215 <choong6215@gmail.com>
2024-05-14 17:27:19 +0900

    전역 스타일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { appContainer, board, buttons } from "./App.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className={appContainer}>
+      <div className={board}></div>
+
+      <div className={buttons}>
+        <button>이 게시판 삭제하기</button>
+        <button>활동 목록 보이기</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
