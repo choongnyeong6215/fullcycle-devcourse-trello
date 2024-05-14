db11281cb8da41537caeb593bf8a08a2223e52c7
choongnyeong6215 <choong6215@gmail.com>
2024-05-14 16:38:13 +0900

    보드 데이터 타입 적용

--- src/store/slices/boardsSlices.ts
+++ src/store/slices/boardsSlices.ts
@@ -1,8 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+interface IBoardState {
+  modalActive: boolean;
+  boardArray: IBoard[];
+}
+
+const initialState: IBoardState = {
   modalActive: false,
-  boardArray: [],
+  boardArray: [
+    {
+      boardId: "board-0",
+      boardName: "첫 번째 게시물",
+      lists: [
+        {
+          listId: "list-0",
+          listName: "List 1",
+          tasks: [
+            {
+              taskId: "task-0",
+              taskName: "Task 1",
+              taskDescription: "Description1",
+              taskOwner: "Leeee",
+            },
+            {
+              taskId: "task-1",
+              taskName: "Task 2",
+              taskDescription: "Description2",
+              taskOwner: "Leeee",
+            },
+          ],
+        },
+        {
+          listId: "list-1",
+          listName: "List 2",
+          tasks: [
+            {
+              taskId: "task-2",
+              taskName: "Task 3",
+              taskDescription: "Description3",
+              taskOwner: "kwak",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 const boardsSlice = createSlice({
