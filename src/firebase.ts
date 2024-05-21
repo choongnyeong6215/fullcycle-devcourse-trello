db5815400a9bf83148cd77c14592b716b3b4877a
choongnyeong6215 <choong6215@gmail.com>
2024-05-21 15:54:23 +0900

    firebase setting

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwIwdsesAF52HLNpQ7ns2g6GHRE_JZS_Q",
+  authDomain: "trello-clone-a2939.firebaseapp.com",
+  projectId: "trello-clone-a2939",
+  storageBucket: "trello-clone-a2939.appspot.com",
+  messagingSenderId: "65978330116",
+  appId: "1:65978330116:web:37a9fd1044f58a6ca45951",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
